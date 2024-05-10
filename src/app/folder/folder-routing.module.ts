73c24a1776cb6035e2d8087949581455a4d9274a
Jesiel Palacios <jopalacioss@unac.edu.co>
2024-05-10 09:52:06 -0500

    Adds app routing

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { HomePage } from '../screens/home/home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
-  }
+    component: FolderPage,
+    children: [
+      {
+        path: 'inicio',
+        loadChildren: () => import('../screens/home/home.module').then(m => m.HomePageModule)
+      }
+    ]
+  },
+  // {
+  //   path: 'inicio',
+  //   component: HomePage
+  // }
 ];
 
 @NgModule({
